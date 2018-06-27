201a54d6cee8aab3281bd1d06a17ec269f38c4dc
Shaun <shaun.apps@gmail.com>
2018-06-26 20:47:11 -0400

    this stuff is interesting to learn

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,53 @@ type Tx = mpsc::UnboundedSender<Bytes>;
 /// Shorthand for the receive half of the message channel.
 type Rx = mpsc::UnboundedReceiver<Bytes>;
 
+struct Lines {
+    socket: TcpStream,
+    rd: BytesMut,
+    wr: BytesMut,
+}
+
+impl Lines {
+    // Create a new 'Lines' codec backed by the socket
+    fn new(socket: TcpStream) -> Self {
+        Lines {
+            socket,
+            rd: BytesMut::new(),
+            wr: BytesMut::new(),
+        }
+    }
+}
+
+struct Shared {
+    peers: HashMap<SocketAddr, Tx>,
+}
+
+impl Shared {
+    /// Create a new, empty, instance of `Shared`.
+    fn new() -> Self {
+        Shared {
+            peers: HashMap::new(),
+        }
+    }
+}
+
+fn process(socket: TcpStream, state: Arc<Mutex<Shared>>) {
+    // Define the task that processes the connection.
+    let task = unimplemented!();
+
+    // Spawn the task
+    tokio::spawn(task);
+}
+
 fn main() {
+    let state = Arc::new(Mutex::new(Shared::new()));
     let addr = "127.0.0.1:6142".parse().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
 
     let server = listener
         .incoming()
         .for_each(move |socket| {
-            // TODO: Process socket
+            process(socket, state.clone());
             Ok(())
         })
         .map_err(|err| {
